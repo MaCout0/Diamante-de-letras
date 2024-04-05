d75ba4f83b4d9f4fde2274af5f0a59adcd5291e3
MaCout0 <jkmbint@gmail.com>
2024-04-05 14:04:55 -0300

    Add project files.

---
+++ Diamante de letras/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace Diamante_de_letras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis do código
+            char letraEscolhida;
+            int espaçoInterno = -1;
+            
+
+            Console.WriteLine("Digite uma letra de A a Z: ");
+            letraEscolhida = Convert.ToChar(Console.ReadLine());
+            letraEscolhida = char.ToUpper(letraEscolhida);
+            Console.ReadLine();
+            Console.Clear();
+
+            //parte superior
+            constroiParteSuperior(letraEscolhida, ref espaçoInterno);
+
+            espaçoInterno -= 4;
+            letraEscolhida--;
+
+            //parte inferior            
+            constroiParteInferior(letraEscolhida, ref espaçoInterno);
+
+            Console.ReadLine();
+        }
+
+        private static void constroiParteInferior(char letraEscolhida, ref int espaçoInterno)
+        {
+            char primeiraLetra = 'A';
+
+            int margem = 1;
+
+            for (char letra = letraEscolhida; letra >= primeiraLetra; letra--)
+            {
+                for (int i = 0; i < margem; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                margem++;
+
+                Console.Write(letra);
+
+                for (int i = 0; i < espaçoInterno; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                espaçoInterno -= 2;
+
+                if (letra == 'A')
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine(letra);
+                }
+            }
+        }
+
+        static void constroiParteSuperior(char letraEscolhida, ref int espaçoInterno)
+        {
+            char primeiraLetra = 'A';
+
+            espaçoInterno = -1;
+
+            int margem = letraEscolhida - primeiraLetra;
+
+            for (char letra = 'A'; letra <= letraEscolhida; letra++)
+            {
+                for (int i = 0; i < margem; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                margem--;
+
+                Console.Write(letra);
+
+                for (int i = 0; i < espaçoInterno; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                espaçoInterno += 2;
+
+                if (letra == 'A')
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine(letra);
+                }
+            }
+        }
+    }
+}
